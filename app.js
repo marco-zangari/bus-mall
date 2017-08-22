9c29fb76c60d37ad05ce11bdcbe6a8f2e2543e47
Marco Zangari <mjzangari@gmail.com>
2017-08-22 13:56:14 -0700

    pulled the methods out of the constructor function; made them functions; got an object to get on the page

--- app.js
+++ app.js
@@ -1,48 +1,51 @@
 'use strict';
 
-var items = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'chthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'waterCan', 'wineGlass'];
+var imageChooserList = [];
+var lastThree = [];
 
-function ImageChooser (name, filePath, numDisplayed, numClicked, textString) {
+function ImageChooser (name, filePath, id) {
     this.name = name;
     this.filePath = filePath;
-    this.numDisplayed = numDisplayed;
-    this.numClicked = numClicked;
-    this.textString = textString;
+    this.id = id;
+    this.numDisplayed = 0;
+    this.numClicked = 0;
+    imageChooserList.push(this);
+    };
 
-    items.push(this);
-
-    this.setRandom = function () {
-        return Math.floor(Math.random() * items.length - 1);
+    var setRandom = function () {
+      return Math.floor(Math.random() * imageChooserList.length);
       };
 
-  this.renderOneImage = function () {
-      var imagePutter = document.getElementById('imageOne')
+    var renderOneImage = function () {
+      var random = setRandom();
+      var imagePutter = document.getElementById('imageOne');
+      imagePutter.innerHTML = ' ';
       var imageOne = document.createElement('img');
-      imageOne.setAttribute('src', this.filepath);
       imagePutter.appendChild(imageOne);
+
+      imageOne.setAttribute('src', bag.filePath)
+
     };
-};
 
+    var bag = new ImageChooser('a bag','img/bag.jpg', 'bag');
+    var banana = new ImageChooser('a banana','img/banana.jpg', 'banana');
+    var bathroom = new ImageChooser('a bathroom', 'img/bathroom.jpg', 'bathroom');
+    var boots = new ImageChooser('the boots', 'img/boots.png', 'boots');
+    var breakfast = new ImageChooser('a breakfast', 'img/breakfast.jpg', 'breakfast');
+    var bubblegum = new ImageChooser('the bubblegum', 'img/bubblegum.jpg', 'bubblegum');
+    var chair = new ImageChooser('a chair', 'img/chair.jpg', 'chair');
+    var cthulhu = new ImageChooser('the cthulhu', 'img/chair.jpg', 'cthulhu');
+    var dogDuck = new ImageChooser('a dog duck', 'img/dog-duck.jpg', 'dogDuck');
+    var dragon = new ImageChooser('a dragon', 'img/dragon.jpg', 'dragon');
+    var pen = new ImageChooser('a pen', 'img/pen.jpg', 'pen');
+    var petSweep = new ImageChooser('a pet sweep', 'img/pet-sweep.jpg', 'petSweep');
+    var scissors = new ImageChooser('the scissors', 'img/scissors.jpg', 'scissors');
+    var shark = new ImageChooser('a shark', 'img/shark.jpg', 'shark');
+    var sweep = new ImageChooser('the sweep', 'img/sweep.png', 'sweep');
+    var tauntaun = new ImageChooser('a tauntaun', 'img/tauntaun.jpg', 'tauntaun');
+    var unicorn = new ImageChooser('the unicorn', 'img/unicorn.jpg', 'unicorn');
+    var usb = new ImageChooser('the usb', 'img/usb.gif', 'usb');
+    var waterCan = new ImageChooser('a water can', 'img/water-can.jpg', 'waterCan');
+    var wineGlass = new ImageChooser('a wine glass', 'img/wine-glass.jpg', 'wineGlass');
 
-var bag = new ImageChooser('bag','/Users/marcozangari/codefellows/201/bus-mall/img/bag.jpg', 0,0);
-var banana = new ImageChooser('banana','img/banana.jpg', 0, 0);
-var bathroom = new ImageChooser('bathroom', 'img/bathroom.jpg', 0, 0);
-var boots = new ImageChooser('boots', 'img/boots.png', 0, 0);
-var breakfast = new ImageChooser('breakfast', 'img/breakfast.jpg', 0, 0);
-var bubblegum = new ImageChooser('bubblegum', 'img/bubblegum.jpg', 0, 0);
-var chair = new ImageChooser('chair', 'img/chair.jpg', 0, 0);
-var cthulhu = new ImageChooser('cthulhu', 'img/chair.jpg', 0, 0);
-var dogDuck = new ImageChooser('dogDuck', 'img/dog-duck.jpg', 0, 0);
-var dragon = new ImageChooser('dragon', 'img/dragon.jpg', 0, 0);
-var pen = new ImageChooser('pen', 'img/pen.jpg', 0, 0);
-var petSweep = new ImageChooser('petSweep', 'img/pet-sweep.jpg', 0, 0);
-var scissors = new ImageChooser('scissors', 'img/scissors.jpg', 0, 0);
-var shark = new ImageChooser('shark', 'img/shark.jpg', 0, 0);
-var sweep = new ImageChooser('sweep', 'img/sweep.png', 0, 0);
-var tauntaun = new ImageChooser('tauntaun', 'img/tauntaun.jpg', 0, 0);
-var unicorn = new ImageChooser('unicorn', 'img/unicorn.jpg', 0, 0);
-var usb = new ImageChooser('usb', 'img/usb.gif', 0, 0);
-var waterCan = new ImageChooser('waterCan', 'img/water-can.jpg', 0, 0);
-var wineGlass = new ImageChooser('wineGlass', 'img/wine-glass.jpg', 0, 0);
-
-bag.renderOneImage();
+renderOneImage();
