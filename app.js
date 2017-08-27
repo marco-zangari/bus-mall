272033559755f3318c49ee83e23b9941eceb30d2
Marco Zangari <mjzangari@gmail.com>
2017-08-26 22:34:00 -0700

    debugging; making the on click

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
-var pageClicked = 0;
+var imageClicked = 0;
 var maxClick = 3;
 var imageChooserList = [];
 var lastThree = [];
 var currentThree = [];
-var idArray = ['imageOne', 'imageTwo', 'imageThree'];
+var idArray = ['image-1', 'image-2', 'image-3'];
 var imagePath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/chair.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
 
 var bag = new ImageChooser('a bag','img/bag.jpg', 'bag');
@@ -28,9 +28,6 @@ var usb = new ImageChooser('the usb', 'img/usb.gif', 'usb');
 var waterCan = new ImageChooser('a water can', 'img/water-can.jpg', 'waterCan');
 var wineGlass = new ImageChooser('a wine glass', 'img/wine-glass.jpg', 'wineGlass');
 
-if (localStorage.getItem('clicks')){
-  var countedClicks = JSON.parse(localStorage.getItem('clicks'));
-}
 
 function ImageChooser (name, filePath, id) {
   this.name = name;
@@ -55,30 +52,43 @@ function renderThreeImages(){
       random = setRandom();
     }
     productImage[i].setAttribute('src', imageChooserList[random].filePath);
+    productImage[i].id = imageChooserList[random].id;
+    imageChooserList[random].numDisplayed++;
     currentThree.push(random);
     }
   lastThree = currentThree;
 
   for (var i = 0; i < document.getElementsByClassName('clickable').length; i++) {
-  var image = document.getElementById('image-' + (i + 1));
-  image.addEventListener('click', onClick);
+    var image = document.getElementById('image-' + (i + 1));
+    image.addEventListener('click', onClickEvent);
   }
 };
 renderThreeImages();
 
 function onClickEvent(event) {
-  var itemIdx = parseInt(event.target.getAttribute('itemIdx'));
-  var userChoice = imageChooserList[itemIdx];
-  userChoice.pageClicked++;
+  for (var i = 0; i < imageChooserList.length; i++) {
+    if (imageChooserList[i].id === event.target.id) {
+        imageChooserList[i].numClicked++;
+        imageClicked++;
+    }
+  };
+
+  numClicked.imageClicked++;
   event.preventDefault();
   renderThreeImages();
   maxClick++;
 
-  if (pageClicked === maxClick) {
-      for (var i = 0; i < document.getElementsByClassName('clickable').length; i++) {
-
-        document.getElementsByClassName('clickable').lei]
+  if (imageClicked === maxClick) {
+    for (var i = 0; i < document.getElementsByClassName('clickable').length; i++) {
+      var image = document.getElementById('image-' + (i +1));
+      image.removeEventListener('click', OnClickEvent);
+        }
+        for (var i = 0; i < imageChooserList.length; i++) {
+          countedClicks.push(imageChooserList[i].timesClicked);
+        }
       }
+  };
+
 
     //   var index = lastThree[0];
     //   imageChooserList[index].numClicked++;
@@ -93,8 +103,8 @@ function onClickEvent(event) {
     // } else {
 
     var myChart = new Chart(ctx, chartConfig);
-    }
-};
+
+
 // clickCounter();
 
 for (var i = 0; i < idArray.length; i++) {
@@ -106,49 +116,49 @@ for (var i = 0; i < idArray.length; i++) {
 // stopThatImageNow();
 
 
-var chartConfig = {
-
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], // x-axis labels for every entry in your data set. It should match up with the number of things you're plotting (if it's a bar chart)
-    datasets: [{ // <-- notice that this can be an array of multiple data sets.
-      // each data set is its own object literal.
-      label: '# of Votes', // <-- the label of this one data set
-      data: [12, 19, 3, 5, 2, 3], // <-- where your data actually goes. just the numbers
-      backgroundColor: [ // <-- this can be either one single color or a color for each item in your bar chart.
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 5 // border width in pixels
-    }]
-  },
-  options: {
-    // maintainAspectRatio: false,
-    // animation: {
-    //   duration: 1000
-    // },
-    title: {
-      display: true,
-      text: 'Some stuff and some junk'
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        }
-      }]
-    }
-  }
-};
+// var chartConfig = {
+//
+//   type: 'bar',
+//   data: {
+//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], // x-axis labels for every entry in your data set. It should match up with the number of things you're plotting (if it's a bar chart)
+//     datasets: [{ // <-- notice that this can be an array of multiple data sets.
+//       // each data set is its own object literal.
+//       label: '# of Votes', // <-- the label of this one data set
+//       data: [12, 19, 3, 5, 2, 3], // <-- where your data actually goes. just the numbers
+//       backgroundColor: [ // <-- this can be either one single color or a color for each item in your bar chart.
+//         'rgba(255, 99, 132, 0.2)',
+//         'rgba(54, 162, 235, 0.2)',
+//         'rgba(255, 206, 86, 0.2)',
+//         'rgba(75, 192, 192, 0.2)',
+//         'rgba(153, 102, 255, 0.2)',
+//         'rgba(255, 159, 64, 0.2)'
+//       ],
+//       borderColor: [
+//         'rgba(255,99,132,1)',
+//         'rgba(54, 162, 235, 1)',
+//         'rgba(255, 206, 86, 1)',
+//         'rgba(75, 192, 192, 1)',
+//         'rgba(153, 102, 255, 1)',
+//         'rgba(255, 159, 64, 1)'
+//       ],
+//       borderWidth: 5 // border width in pixels
+//     }]
+//   },
+//   options: {
+//     // maintainAspectRatio: false,
+//     // animation: {
+//     //   duration: 1000
+//     // },
+//     title: {
+//       display: true,
+//       text: 'Some stuff and some junk'
+//     },
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           beginAtZero:true
+//         }
+//       }]
+//     }
+//   }
+// };
